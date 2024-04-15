659321fd2fb0a64072a8962b3b4a06fe1c23e40c
Mubashir-rehma <rmubashir03@gmail.com>
2024-04-15 13:21:58 +0500

    some css modified

--- gd_payments.php
+++ gd_payments.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="shortcut icon" href="./Assets/Images/WhatsApp Image 2022-05-16 at 2.20 1.png" type="image/x-icon">
+    <!-- <link rel="shortcut icon" href="./Assets/Images/WhatsApp Image 2022-05-16 at 2.20 1.png" type="image/x-icon"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -70,126 +70,6 @@ $gds = mysqli_fetch_array($gd);
 
 $query1 = 'SELECT SUM(total_paid) AS total_paid FROM gdpays';
 $result1 = $mysqli->query($query1);
-// if (!empty($_GET['id'])) {
-//     $id = $_GET['id'];
-//     $newloaddata = $mysqli->query("SELECT * FROM newload n 
-//     LEFT OUTER JOIN truck_details AS t ON t.truck_id = n.truck_Number 
-//     LEFT OUTER JOIN broker_details AS b ON b.broker_id = n.Broker 
-//     LEFT OUTER JOIN (select *, max(callid) from newcheckcalls 
-//     GROUP BY newloadID) C ON C.newloadID = n.id where n.id = '$id'
-//     ORDER BY id DESC limit 1") or die($mysqli->error);
-
-//     $newloaddata = mysqli_fetch_assoc($newloaddata);
-// };
-
-
-// $address = $mysqli->query("SELECT * FROM address") or die($mysqli->error);
-// $dest = $mysqli->query("SELECT * FROM address") or die($mysqli->error);
-// $truck_details = $mysqli->query("SELECT * FROM truck_details") or die($mysqli->error);
-// $broker_details = $mysqli->query("SELECT * FROM broker_details GROUP BY broker_company") or die($mysqli->error);
-// $users = $mysqli->query("SELECT user_name FROM users") or die($mysqli->error);
-
-// if (!empty($_GET['callid'])) {
-//     $callid = $_GET['callid'];
-//     $checkcalls = $mysqli->query("SELECT * FROM newcheckcalls where callid=$callid") or die($mysqli->error);
-//     $checkcall = mysqli_fetch_assoc($checkcalls);
-// }
-
-
-// // Pre-filled data 
-// $newloadData = !empty($postData) ? $postData : $newloadData;
-
-// // Define action 
-// $actionLabel = !empty($_GET['id']) ? 'Update' : 'Add';
-
-// $load_en_routestatus = $mysqli->query("SELECT count(status) FROM newload where status='load_en_route'") or die($mysqli->error);
-// $load_en_routestatusData = mysqli_fetch_assoc($load_en_routestatus);
-// $load_en_route = $load_en_routestatusData['count(status)'];
-
-// $load_deliveredstatus = $mysqli->query("SELECT count(status) FROM newload where status='load_delivered'") or die($mysqli->error);
-// $load_deliveredstatusData = mysqli_fetch_assoc($load_deliveredstatus);
-// $load_delivered = $load_deliveredstatusData['count(status)'];
-
-// $load_issuestatus = $mysqli->query("SELECT count(status) FROM newload where status='load_issue'") or die($mysqli->error);
-// $load_issuestatusData = mysqli_fetch_assoc($load_issuestatus);
-// $load_issue = $load_issuestatusData['count(status)'];
-
-// $load_invoicedstatus = $mysqli->query("SELECT count(status) FROM newload where status='load_invoiced'") or die($mysqli->error);
-// $load_invoicedstatusData = mysqli_fetch_assoc($load_invoicedstatus);
-// $load_invoiced = $load_invoicedstatusData['count(status)'];
-
-// $load_paidstatus = $mysqli->query("SELECT count(status) FROM newload where status='load_paid'") or die($mysqli->error);
-// $load_paidstatusData = mysqli_fetch_assoc($load_paidstatus);
-// $load_paid = $load_paidstatusData['count(status)'];
-
-// $load_factored = $mysqli->query("SELECT count(status) FROM newload where status='load_Factored'") or die($mysqli->error);
-// $load_factoredData = mysqli_fetch_assoc($load_factored);
-// $load_factored = $load_factoredData['count(status)'];
-
-// // Number of total loads delivered today
-// $today = date('Y-m-d');
-// $loads_delivered_todayQuery = $mysqli->query("SELECT count(status) FROM newload where DATE(delivery_date)='$today'") or die($mysqli->error);
-// $loads_delivered_todayQueryData = mysqli_fetch_assoc($loads_delivered_todayQuery);
-// $loads_delivered_today = $loads_delivered_todayQueryData['count(status)'];
-
-// $disQuery = $mysqli->query("SELECT dispatcher,count(dispatcher)  AS count_me FROM newload WHERE dispatcher IS NOT NULL GROUP BY dispatcher ORDER BY COUNT(dispatcher) DESC") or die($mysqli->error);
-// $disData = mysqli_fetch_array($disQuery);
-
-
-// $status_bar_query = "select * from newload as n 
-// left outer join 
-// (select * from load_tracking T where exists (select * from (select load_id, max(tracking_id) as tracking_id
-//     from load_tracking tt 
-//     group by Load_pickup_location, load_Destination
-//     ) as tt where T.tracking_id = tt.tracking_id)) T 
-//     on n.id = T.load_id 
-// LEFT OUTER JOIN truck_details AS t 
-//     ON t.truck_id = n.truck_Number 
-// LEFT OUTER JOIN broker_details AS b 
-//     ON b.broker_id = n.Broker 
-// LEFT OUTER JOIN 
-//     (select * from newcheckcalls C where exists (select * from (select newloadID, max(callid) as callid
-//     from newcheckcalls cc 
-//     GROUP BY newloadID
-//     ) as cc where C.callid = cc.callid)) C on n.id = C.newloadID 
-// where n.status = 'load_en_route'
-// order by n.id desc";
-// $status_bar = $mysqli->query($status_bar_query) or die($mysqli->error);
-// foreach($status_bar as $row){
-//     print("  lID:   ". $row['id'] . "  TID:  " . $row['tracking_id'] . "  LPU:  " . $row['Pick_up_Location'] . "  TPU:   " . $row['Load_pickup_location'] . "  LDES:  " . $row['Destination'] . "   TDES:  " .$row['load_Destination'] . "  Ttotal DIS:   " . $row['total_distance'] . "  TC DIS:   " .$row['current_distace'] . "<br>");
-// };
-
-// function loadstatusbars($mysqli)
-// {
-//     $query = "select * from newload where status <> 'load_delivered' order by id desc";
-//     $data = $mysqli->query($query) or die($mysqli->error);
-
-//     foreach ($data as $row) {
-//         $PU = unserialize($row['Pick_up_Location']);
-//         $des = unserialize($row['Destination']);
-//         $dis = unserialize($row['distance']);
-//         $time = unserialize($row['time']);
-//         $id = $row['id'];
-
-//         $count = count(is_countable($PU) ? $PU : []);
-
-//         print("count:   " .  $count  . "  /PU:  " . $row['Pick_up_Location'] . "  /des:  " . $row['Destination'] . "   /dis:  " . $row['distance'] . "  /time:  " . $row['time'] . "<br>");
-
-//         for ($i = 0; $i < $count; $i++) {
-//             $PU = $PU[$i];
-//             $des = $des[$i];
-//             $dis = $dis[$i];
-//             $time = $time[$i];
-
-//             $tquery = "select * from load_tracking where load_id='$id' and load_Destination='$des' and Load_pickup_location='$PU' order by tracking_id desc limit 1";
-//             $tdata = $mysqli->query($tquery) or die($mysqli->error);
-
-//             foreach ($tdata as $t) {
-//                 // print("  lID:   " . $id . "  TID:  " . $t['tracking_id'] . "  LPU:  " . $PU . "  TPU:   " . $t['Load_pickup_location'] . "  LDES:  " . $des . "   TDES:  " . $t['load_Destination'] . "  Ttotal DIS:   " . $dis . "  TC DIS:   " . $t['current_distace'] . "   T CL:  " . $t['current_location'] . "<br>");
-//             }
-//         };
-//     };
-// }
 
 
 
